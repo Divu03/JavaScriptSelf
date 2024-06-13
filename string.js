f40ddefcc4aa3b70f772019deab4b3bb67b9903e
Divu <divyeshkuchhadia@gmail.com>
2024-06-13 13:28:15 +0530

    charAt codePointAt concate

--- string.js
+++ string.js
@@ -14,3 +14,12 @@ console.log(ccstr);
 
 let ssstr = str.substring(2,5);
 console.log(ssstr);
+
+let castr = str.charAt(2);
+console.log(castr);
+
+let cpastr = str.codePointAt(2);
+console.log(cpastr);
+
+let cstr = str.concat("hi from my side","second string");
+console.log(cstr);
