3df85e75353216023202271f38009550dd9b22ff
Divu <divyeshkuchhadia@gmail.com>
2024-06-13 12:41:01 +0530

    split method 2 and length

--- string.js
+++ string.js
@@ -1,6 +1,10 @@
 let str = "  o v b ";
 
 let rstr = str.replace("o","m");
-rstr = str.split();
+console.log(rstr);
 
+let sstr = str.split("v");
+console.log(sstr);
+
+rstr = str.length;
 console.log(rstr);
